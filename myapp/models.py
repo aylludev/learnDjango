b51f081ddfc097cc8b7c95f43c67af4a22e4d9cd
ayllude <cafo1995@hotmail.com>
2024-08-11 00:42:21 -0500

    models

--- myapp/models.py
+++ myapp/models.py
@@ -16,6 +16,7 @@ class Category(models.Model):
         ordering = ['id']
 
 class Product(models.Model):
+    id_pro = models.CharField(primary_key=True, max_length=10, unique=True, verbose_name='Id_producto')
     name = models.CharField(max_length=150, verbose_name='Nombre',unique=True)
     cate = models.ForeignKey(Category,on_delete=models.CASCADE)
     image = models.ImageField(upload_to='product/%Y/%m/%d', null=True, blank=True)
@@ -29,37 +30,57 @@ class Product(models.Model):
         verbose_name = 'Producto'
         verbose_name_plural = 'Productos'
         db_table = 'producto'
-        ordering = ['id']
+        ordering = ['id_pro']
 
-class Type(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Tipo')
-
-    def __str__(self):
-        return self.name
-    
-    class Meta:
-        verbose_name = 'Tipo'
-        verbose_name_plural = 'Tipos'
-        db_table = 'tipo'
-        ordering = ['id']
-
-class Employee(models.Model):
-    id = models.CharField(primary_key=True, max_length=10, unique=True, verbose_name='Cedula')
+class Client(models.Model):
+    id_cli = models.CharField(primary_key=True, max_length=10, unique=True, verbose_name='Cedula')
     names = models.CharField(max_length=150, verbose_name='Nombres')
-    type = models.ForeignKey(Type, on_delete=models.PROTECT)
-    date_joined = models.DateField(default=datetime.now, verbose_name='Nombre de registro')
-    date_created= models.DateTimeField(auto_now=True)
-    age = models.PositiveIntegerField(default=0)
-    salary = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
-    state = models.BooleanField(default=True)
-    avatar = models.ImageField(upload_to='avatar')
-    cvitae = models.FileField(upload_to='cvitae/%Y/%m/%d', null=True, blank=True)
+    lastnames = models.CharField(max_length=150, verbose_name='Apellidos')
+    date_nac= models.DateTimeField(auto_now=True)
+    direction = models.CharField(max_length=150, verbose_name='Direccion')
+    gendre = models.CharField(max_length=150, verbose_name='Genero')
+
 
     def __str__(self):
         return self.names
     
     class Meta:
-        verbose_name = 'Empleado'
-        verbose_name_plural = 'Empleados'
-        db_table = 'empleado'
-        ordering= ['id']
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+        db_table = 'clientes'
+        ordering= ['id_cli']
+
+class Sale(models.Model):
+    id_sale = models.CharField(primary_key=True, max_length=10, unique=True, verbose_name='Id_venta')
+    id_cli = models.ForeignKey(Client, on_delete=models.CASCADE)
+    date_sale= models.DateTimeField(auto_now=True)
+    subtotal = models.FloatField()
+    iva = models.FloatField()
+    total = models.FloatField()
+    
+
+    def __str__(self):
+        return self.id_sale
+    
+    class Meta:
+        verbose_name = 'Venta'
+        verbose_name_plural = 'Ventas'
+        db_table = 'ventas'
+        ordering= ['id_sale']
+
+class DetSale(models.Model):
+    id_det = models.CharField(primary_key=True, max_length=10, unique=True, verbose_name='Id_venta')
+    id_sale = models.ForeignKey(Sale, on_delete=models.CASCADE)
+    id_pro = models.CharField(Product)
+    quanty = models.FloatField()
+    price = models.FloatField()
+    subtotal = models.FloatField()    
+
+    def __str__(self):
+        return self.id_sale
+    
+    class Meta:
+        verbose_name = 'DetVenta'
+        verbose_name_plural = 'DetVentas'
+        db_table = 'detventas'
+        ordering= ['id_det']
--- myproject/urls.py
+++ myproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import myfirstview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('prueba/', myfirstview)
 ]
