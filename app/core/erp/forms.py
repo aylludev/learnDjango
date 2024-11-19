6dad1e5fa392a24a8dfda495ea93c983629426b0
aylludev <cafo1995@hotmail.com>
2024-11-19 16:40:29 -0500

    28

--- app/core/erp/forms.py
+++ app/core/erp/forms.py
@@ -7,9 +7,9 @@ class CategoryForm(ModelForm):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for form in self.visible_fields():
-            form.field.widget.attrs['class'] = 'form-control'
-            form.field.widget.attrs['autocomplete'] = 'off'
+        #for form in self.visible_fields():
+            #form.field.widget.attrs['class'] = 'form-control'
+            #form.field.widget.attrs['autocomplete'] = 'off'
         self.fields['name'].widget.attrs['autofocus'] = True
 
     class Meta:
--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
+from django.template import context
+from django.urls import is_valid_path, reverse_lazy
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
 from django.views.generic import ListView, CreateView
 from django.utils.decorators import method_decorator
@@ -37,16 +38,32 @@ class CategoryListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Listado de Categorías'
+        context['create_url'] = reverse_lazy('erp:category_create')
+        context['list_url'] = reverse_lazy('erp:category_list')
+        context['entity'] = 'Categorias'
         return context
 
 
 class CategoryCreateView(CreateView):
-   model = Category
-   form_class = CategoryForm
-   template_name = 'category/create.html'
-   success_url = reverse_lazy('erp:category_list')
+    model = Category
+    form_class = CategoryForm
+    template_name = 'category/create.html'
+    success_url = reverse_lazy('erp:category_list')
 
-   def get_context_data(self, **kwargs):
-       context = super().get_context_data(**kwargs)
-       context['title'] = 'Creación una Categoria'
-       return context
+    def post(self, request, *args, **kwargs):
+        print(request.POST)
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(self.success_url)
+        self.object = None
+        context = self.get_context_data(**kwargs)
+        context['form'] = form
+        return render(request, self.template_name, context)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Creación una Categoria'
+        context['entity'] = 'Categorias'
+        context['list_url'] = reverse_lazy('erp:category_list')
+        return context
