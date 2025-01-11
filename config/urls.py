c2abda4085fb549445a19cfc67cf234e435c3176
aylludev <cafo1995@hotmail.com>
2025-01-10 21:05:56 -0500

    37

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core.homepage.views import IndexView
+from core.login.views import LoginFormView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginFormView.as_view()),
     path('erp/', include('core.erp.urls')),
+    path('', IndexView.as_view()),
 ]
